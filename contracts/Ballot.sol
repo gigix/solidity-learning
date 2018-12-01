a68f28eda2223030d779e8e57a575b91c63a34ff
Jeff Xiong <gigix@Jeff-Xiongs-MacBook-Pro.local>
2018-12-01 15:03:30 +0800

    basic logic

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -1,9 +1,35 @@
 pragma solidity ^0.4.4;
 
 contract Ballot {
-    uint public numberOfProposals;
+    struct Proposal {
+        uint id;
+        uint voteCount;
+    }
+
+    Proposal [] proposals;
 
     constructor(uint _numberOfProposals) public {
-        numberOfProposals = _numberOfProposals;
+        proposals.length = _numberOfProposals;
+        for(uint i = 0; i < _numberOfProposals; i++) {
+            proposals[i].id = i;
+        }
+    }
+
+    function numberOfProposals() constant public returns(uint) {
+        return proposals.length;
+    }
+
+    function vote(uint _indexOfProposal) public {
+        proposals[_indexOfProposal].voteCount ++;
+    }
+
+    function winningProposal() constant public returns(uint) {
+        uint candidate = 0;
+        for(uint i = 0; i < numberOfProposals(); i++) {
+            if(proposals[i].voteCount > proposals[candidate].voteCount) {
+                candidate = i;
+            }
+        }
+        return proposals[candidate].id;
     }
 }
--- test/BallotTest.sol
+++ test/BallotTest.sol
@@ -5,12 +5,22 @@ import "truffle/Assert.sol";
 import "../contracts/Ballot.sol";
 
 contract BallotTest {
+    Ballot ballot;
+
+    function beforeEach() public {
+        ballot = new Ballot(3);
+    }
+
     function test_trivial_test_case_should_run() public {
         Assert.equal(uint(1), uint(1), "Assertion should work");
     }
 
     function test_ballot_can_be_initiated() public {
-        Ballot ballot = new Ballot(3);
         Assert.equal(ballot.numberOfProposals(), 3, "Number of proposals should be as initiated");
+    }
+
+    function test_ballot_can_be_voted() public {
+        ballot.vote(1);
+        Assert.equal(ballot.winningProposal(), 1, "Voted proposal should win");
     }
 }
